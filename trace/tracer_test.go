477990cd2ceb6f6c7d9d4a2aef6f207093b31390
Aren Khachatoorian <khachatoorianaren@gmail.com>
2026-01-01 17:48:24 +0400

    Tracer initial implementation

---
+++ trace/tracer_test.go
@@ -0,0 +1,24 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil {
+		t.Fatal("return from New should not be nil")
+	}
+
+	traceMsg := "Hello trace package."
+	expected := traceMsg + "\n"
+
+	tracer.Trace(traceMsg)
+
+	if got := buf.String(); got != expected {
+		t.Errorf("Trace wrote: %q, expected: %q", got, expected)
+	}
+}
